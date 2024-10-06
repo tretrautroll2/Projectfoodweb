8d7cc9dab213593a539548af5895237e8ddf57f3
tretrautroll2 <ninhducnhatanhsucvat@gmail.com>
2024-10-06 21:54:01 +0700

    Update Item Modal to Home component

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,14 +4,17 @@
 import {AllFoodItems} from "./Food";
 import {AllDrinksItems} from "./Drinks";
 import {AllOtherItems} from "./Others";
+import ItemDescription from "./ItemInteract";
+import { useState } from "react";
 export const Allitems =  [...AllFoodItems, ...AllDrinksItems, ...AllOtherItems]
 export default function Home(){
-    
+    const [openModal, setOpenModal] = useState(false);
         return(
         <>
         <ul className="container">
         {Allitems.map((item, index) => (<li key={index} className='item'>
-        <div className="item-center">
+        <div onClick={() => setOpenModal(true)} 
+            className="item-center">
             <img className='img' src={item.image} alt="ShitImage"></img>
             <h2>{item.name}</h2>
             <p>Price: {item.price}</p>
@@ -20,6 +23,7 @@ export default function Home(){
         </li>
         ))}
         </ul>
+        <ItemDescription open={openModal} onClose={() => setOpenModal(false)} description={"This is the product description"} /> 
         </>
     )
 }
