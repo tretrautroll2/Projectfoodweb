b833fe05de683a9cd35b90e86546703f4d0e0623
tretrautroll2 <ninhducnhatanhsucvat@gmail.com>
2024-10-07 16:13:16 +0700

    Update shortened useState for Modal

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -6,17 +6,13 @@ import {AllDrinksItems} from "./Drinks";
 import {AllOtherItems} from "./Others";
 import ItemDescription from "./ItemDescription";
 import { useState } from "react";
+import { Router, Link } from "react-router-dom";
 export const Allitems =  [...AllFoodItems, ...AllDrinksItems, ...AllOtherItems]
 export default function Home(){
-    const [openModal, setOpenModal] = useState(false);
     const [selectedItem, setSelectedItem] = useState(null);
-    const handleItemClick = (item) => {  
-    console.log("Item clicked:", item);
-     setSelectedItem(item);
-     setOpenModal(true); 
-    };
-    console.log("Modal open:", openModal);
-    console.log("Selected item:", selectedItem);
+    const handleItemClick = (item) => {
+        setSelectedItem(item);
+    }
         return(
         <>
         <ul className="container">
@@ -26,14 +22,14 @@ export default function Home(){
             <img className='img' src={item.image} alt="ShitImage"></img>
             <h2>{item.name}</h2>
             <p>Price: {item.price}</p>
-          
-        </div>  
+       
+        </div>       
             <button>Add to cart</button>  
         </li>
         ))}
         </ul>
-        {openModal && selectedItem &&(
-        <ItemDescription open={openModal} onClose={() => setOpenModal(false)}
+        {selectedItem &&(
+        <ItemDescription open={selectedItem} onClose={() => setSelectedItem(null)}
              title={selectedItem.name} description={selectedItem.details} /> 
         )}
         </>
