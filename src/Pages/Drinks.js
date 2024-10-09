71f72df55b5abe7c9c291e9785e4b36e341c0be6
tretrautroll2 <ninhducnhatanhsucvat@gmail.com>
2024-10-09 17:06:59 +0700

    Added Dollar sign

--- src/Pages/Drinks.js
+++ src/Pages/Drinks.js
@@ -55,7 +55,7 @@ const { addToCart, cartItems } = useCart();
           className='item-center no-link'>
           <img className='img' src={item.image} alt="ShitImage"></img>
           <h2>{item.name}</h2>
-          <p>Price: {item.price}</p>
+          <p>Price: ${item.price}</p>
           </Link>
           <button onClick={() => addToCart(item.id)}>Add to cart {cartItems[item.id] > 0 && <>({cartItems[item.id]})</>}</button>  
       </li>
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -18,7 +18,7 @@ export default function Home(){
             <Link to={`/item/${item.id}`} className="item-center no-link">
             <img className='img' src={item.image} alt="ShitImage"></img>
             <h2>{item.name}</h2>
-            <p>Price: {item.price}</p>
+            <p>Price: ${item.price}</p>
             </Link>
             <button onClick={() => addToCart(item.id)}>Add to cart {cartItems[item.id] > 0 && <>({cartItems[item.id]})</>}</button>  
         </li>
